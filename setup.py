0675f1f04d1802e7dc79306b8c9ebb21b003c625
ewkoll <ideath@operatorwrold.com>
2023-09-20 16:34:29 +0800

    init

---
+++ setup.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+'''
+Author: ewkoll ideath@operatorwrold.com
+Date: 2023-09-18 19:40:37
+LastEditors: ewkoll
+LastEditTime: 2023-09-18 20:17:49
+Description: 
+Copyright (c) 2023 by ewkoll email: ideath@operatorwrold.com, All Rights Reserved.
+'''
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="koca",
+    version="0.0.1",
+    author="ewkoll",
+    author_email="ideath@operatorworld.com",
+    description="A small koca package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
